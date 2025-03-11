8d307225fbfbc38e23185427a70d96204b6e8596
MK <99416359+MK316@users.noreply.github.com>
2025-03-12 00:16:31 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,29 +1,31 @@
 import streamlit as st
 
+# Display the application's image or logo if available
+url = "https://github.com/MK316/APP4U/raw/main/images/cat02.png"
+st.image(url, caption='App4U - Empowering English Educators', width=200)
+
 # Title of your application
-st.title('Welcome to App4U!')
+st.markdown('### Welcome to App4U!')
 
 # Description of the application
-st.header('Customized Learning for Future English Teachers')
+st.caption('Customized Learning for Future English Teachers')
 st.write("""
 App4U is dedicated to supporting prospective English teachers by providing tailored learning tools focused on phonetics/phonology studies and Teacher Certificate exam preparation.
 
-### What is offered:
+#### 🍏 What is offered:
 - **Interactive Phonetics and Phonology Tools:** Enhance your understanding of sound patterns and articulation.
 - **Exam Preparation Resources:** Get ready for your Teacher Certification exams with targeted practice and study aids.
 
-### Continuous Updates:
+#### 🍏 Continuous Updates:
 This web blog is continuously updated to reflect your requests and ideas. We value your feedback and encourage you to share your thoughts and needs, helping us to tailor the resources to better suit your preparation journey.
 
-### Stay Tuned:
+#### 🍏 Stay Tuned:
 Keep checking back for the latest tools and updates. We're committed to enhancing your learning experience and helping you succeed in your teaching career!
 
-### Share Your Feedback:
+### 🍏 Share Your Feedback:
 Got ideas or requests? We'd love to hear from you! Your input is crucial in shaping this platform to serve you better.
 """)
 st.write(' ⬅️ Click "Message Board" menu on the left to leave your message')
 
 
-# Display the application's image or logo if available
-url = "https://github.com/MK316/APP4U/raw/main/images/cat02.png"
-st.image(url, caption='App4U - Empowering English Educators', width=200)
+
