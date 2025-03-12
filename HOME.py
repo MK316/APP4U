22d6eeef713d7eb9019ad26555db4138bf45a663
MK <99416359+MK316@users.noreply.github.com>
2025-03-12 12:47:56 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 url = "https://github.com/MK316/APP4U/raw/main/images/apps4U_logo2.png"
 
-st.image(url, caption="MK316 Since Mar.11, 2025", width=600)
+st.image(url, caption="MK316: This app blog opened on Mar.11, 2025", width=600)
