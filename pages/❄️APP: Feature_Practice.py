0dcd7292c4d08e3247e3dc2156d9c55530071455
MK <99416359+MK316@users.noreply.github.com>
2025-03-12 13:33:44 +0900

    Update ❄️APP: Feature_Practice.py

--- pages/❄️APP: Feature_Practice.py
+++ pages/❄️APP: Feature_Practice.py
@@ -113,8 +113,10 @@ with tab3:
     def app():
         st.markdown('#### 🐣 Vowel Feature Matrix')
         st.write('This matrix displays the distinctive features for English vowels in IPA.')
-        st.info("Note 1: [ʌ] is phonologically marked as [+back] even though it is phonetically pronounced as central. This may not be so important for TCE exam.")
-        st.info("Note 2: [ɔ] is marked as [+tense] here. You may find different descriptions depending on textbooks.")
+        st.info("Note 1: The vowel [ʌ] is phonologically marked as [+back], even though it is phonetically pronounced more centrally. This distinction may not be crucial for the TCE exam.")
+        
+        st.info("Note 2: The vowel [ɔ] is marked as [+tense] here. You may encounter different descriptions in various textbooks.")
+
         # Generate the feature matrix DataFrame
         feature_matrix = create_feature_matrix(vowel_features)
     
