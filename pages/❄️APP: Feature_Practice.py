02ff8866bfd8617a785d68d8ebc12eb971823ef6
MK <99416359+MK316@users.noreply.github.com>
2025-03-12 12:34:16 +0900

    Update ❄️APP: Feature_Practice.py

--- pages/❄️APP: Feature_Practice.py
+++ pages/❄️APP: Feature_Practice.py
@@ -46,7 +46,7 @@ with tab1:
         feature_matrix = create_feature_matrix(ipa_features)
     
         # Display the feature matrix
-        st.dataframe(feature_matrix, height=450, use_container_width=True)
+        st.dataframe(feature_matrix, use_container_width=True)
     
     if __name__ == "__main__":
         app()
