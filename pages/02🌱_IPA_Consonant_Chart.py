5848161823b66a24989a20217a5b770391d34d10
MK <99416359+MK316@users.noreply.github.com>
2025-12-16 10:40:18 +0900

    Update 02🌱_IPA_Consonant_Chart.py

--- pages/02🌱_IPA_Consonant_Chart.py
+++ pages/02🌱_IPA_Consonant_Chart.py
@@ -15,72 +15,8 @@ with tab1:
 
 
 with tab2:
-    st.markdown("""
-    <style>
-    table {
-        border-collapse: collapse;
-        margin-top: 1rem;
-        width: 600px;
-    }
-    td {
-        border: none;
-        padding: 0.8em;
-        text-align: center;
-        vertical-align: middle;
-        font-size: 1.3em;
-    }
-    th {
-        border: none;
-        padding: 0.8em;
-        font-size: 1.3em;
-        font-weight: bold;
-    }
-    .orange {
-        color: red;
-        font-weight: bold;
-    }
-    .rowlabel {
-        text-align: center;
-        vertical-align: middle;
-        font-weight: bold;
-    }
-    .bottomcell {
-        vertical-align: bottom;
-        height: 4em;
-    }
-    </style>
+    st.markdown("To be updated.")
 
-    <table>
-        <thead>
-            <tr>
-                <th style="text-align:center; vertical-align:middle;"></th>
-                <th style="text-align:center; vertical-align:middle;">Front</th>
-                <th style="text-align:center; vertical-align:middle;">(Central)</th>
-                <th style="text-align:center; vertical-align:middle;">Back</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="rowlabel">High</td>
-                <td><span class="orange">i</span><br>ɪ</td>
-                <td></td>
-                <td><span class="orange">u</span><br>ʊ</td>
-            </tr>
-            <tr>
-                <td class="rowlabel">(Mid)</td>
-                <td>e<br>ɛ</td>
-                <td>ə<br>ʌ (ɜ)</td>
-                <td>o<br><span class="orange">(ɔ)</span></td>
-            </tr>
-            <tr>
-                <td class="rowlabel">Low</td>
-                <td>æ</td>
-                <td class="bottomcell">a</td>
-                <td class="bottomcell"><span class="orange">ɑ / ɒ</span></td>
-            </tr>
-        </tbody>
-    </table>
-    """, unsafe_allow_html=True)
 
 with tab3:
-    st.markdown("📌 You can add more vowel visualizations or explanation notes here.")
+    st.markdown("📌 To be updated.")
