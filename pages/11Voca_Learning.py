944d4097d8fa0a787fb3b70da0ab0a6531a1a7e7
MK <99416359+MK316@users.noreply.github.com>
2026-03-25 15:06:55 +0900

    Update and rename 11Voca_Learning.py to 11🌱_Voca_Learning_apps.py

--- pages/11Voca_Learning.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.markdown("[md33](https://md33.lovable.app/): md33 dictionary for your practice")
---
+++ pages/11🌱_Voca_Learning_apps.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.markdown("1. [md33](https://md33.lovable.app/): md33 dictionary for your practice")
